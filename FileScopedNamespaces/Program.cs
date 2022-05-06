aa064f3299979a021c651ee946e4572d129c5f86
bbnguyenbb <bbnguyenbb@live.com>
2022-05-06 17:49:46 +0700

    change finish structure solution

---
+++ FileScopedNamespaces/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//có thể bỏ luôn vì đây là namespace mặc định của project
+//namespace FileScopedNamespaces;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("ahihi");
+    }
+}
+
+
---
+++ FileScopedNamespaces/Run.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileScopedNamespaces;
+
+internal class Run
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("run");
+    }
+}
